3740a136c02cb1092e8baf66643c769aed1d3602
Kahraman <qehremanesedli@gmail.com>
2024-04-26 10:50:59 +0300

    player

---
+++ src/Player.java
@@ -0,0 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
+class Player {
+    private int x;
+    private int y;
+    private int life;
+
+    public Player(int initialX, int initialY, int initialLife) {
+        this.x = initialX;
+        this.y = initialY;
+        this.life = initialLife;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void
+}
